8343ba8bcdd1571daa6fc29c6928474b234180fa
merencia <lucas.merencia@gmail.com>
2018-08-07 14:22:58 -0300

    WIP: added tests and fix worker, runner and daemon

--- test/runner-test.js
+++
@@ -1,9 +0,0 @@
-const runner = require('../src/runner');
-const config = '../test/test_assets/confing.json'
-
-
-// runner.register({
-//     "name": "Dummy Task",
-//     "path": "../test/test_assets/dummy_task.js",
-//     "cron": "* * * * *"
-// });
