fa33ef55ba1fa46fb23388dbd7220ded80174435
merencia <lucas@merencia.com>
2025-07-23 20:16:55 -0300

    added db commands

--- packages/backends/mysql/test/testsuite.test.ts
+++ packages/backends/mysql/test/testsuite.test.ts
@@ -1,6 +1,6 @@
 import { testBackend } from "@sidequest/backend-test";
 import MysqlBackend from "../src/mysql-backend";
 
-const connection = process.env.MYSQL_URL ?? "mysql://root:mysql@localhost:3306/sidequest_dev";
+const connection = process.env.MYSQL_URL ?? "mysql://root:mysql@localhost:3306/testdb";
 
 testBackend(() => new MysqlBackend(connection));
