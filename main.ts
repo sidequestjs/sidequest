6325adfec207ad74f5cb5f570c6eeb58b6357037
Giovani Guizzo <giovaniguizzo@gmail.com>
2025-07-23 20:16:55 -0300

    chore: removed trash files

--- main.ts
+++
@@ -1,5 +0,0 @@
-import { Sidequest } from "sidequest";
-import { MyJob } from "./main_job.js";
-
-await Sidequest.start();
-await Sidequest.build(MyJob).availableAt(new Date(9999999999999)).enqueue();
