b15bd6f26c027c87fc03841b44911215e23d8cdf
merencia <lucas.merencia@gmail.com>
2025-07-23 20:16:52 -0300

    test execution

--- test/master-worker-test.js
+++ test/master-worker-test.js
@@ -1,16 +1,32 @@
 const MasterWorker = require('../src/master-worker');
 const { assert } = require('chai');
 const cpus = require('os').cpus().length;
+const fs = require('fs');
 
 describe('MasterWorker', () => {
     let masterWorker;
+    let testFile = ".temp_test.txt";
     
     beforeEach(() => {
+        if(fs.existsSync(testFile)){
+            fs.unlinkSync(testFile, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            });
+        }
         masterWorker = new MasterWorker();
     });
     
     afterEach(() => {
         masterWorker.terminate();
+        if(fs.existsSync(testFile)){
+            fs.unlinkSync(testFile, function(err) {
+                if(err) {
+                    return console.log(err);
+                }
+            });
+        }
     });
     
     it('should generate schedulers', () => {
@@ -23,11 +39,25 @@ describe('MasterWorker', () => {
             assert.lengthOf(tasks, 1);
             done();
         });
-
+        
         masterWorker.register( {
             "name": "Dummy Task",
             "path": "./test/test_assets/dummy_task.js",
             "cron": "* * * * *"
         });
+    });
+    
+    it('should execute a task', (done) => {
+        masterWorker.register( {
+            "name": "Write File",
+            "path": "./test/test_assets/write_file.js",
+            "cron": "* * * * * *"
+        });
+        
+        setTimeout(() => {
+            content = fs.readFileSync(testFile);
+            assert.equal(content, "Temp File"); 
+            done();
+        }, 2000);
     });
 });
