5a048808daba119b2ae9dc26a3ebdbe637c59646
merencia <lucas.merencia@gmail.com>
2025-07-23 20:16:52 -0300

    worker encapsulating a fork proccess

---
+++ test/runner-test.js
@@ -0,0 +1,9 @@
+const runner = require('../src/runner');
+const config = '../test/test_assets/confing.json'
+
+
+// runner.register({
+//     "name": "Dummy Task",
+//     "path": "../test/test_assets/dummy_task.js",
+//     "cron": "* * * * *"
+// });
---
+++ test/worker/worker-test.js
@@ -0,0 +1,47 @@
+const { assert } = require('chai');
+const Worker = require('../../src/worker');
+const path = require('path');
+
+describe('Worker', () => {
+    let worker;
+    
+    beforeEach(() => {
+        worker = new Worker();
+    });
+    
+    afterEach(() => {
+        if(worker.isAlive())
+        worker.kill();
+    });
+    
+    it('should be alive', () => {
+        assert.isTrue(worker.isAlive());
+        assert.isFalse(worker.isDead());
+    });
+    
+    it('should be killed', () => {
+        worker.kill();
+        assert.isTrue(worker.isDead());
+        assert.isFalse(worker.isAlive());
+    });
+    
+    it('should register a task', (done)=>{
+
+        task = {
+            "name": "Dummy Task",
+            "path": path.resolve(__dirname, '../test_assets/dummy_task.js'),
+            "cron": "* * * * *"
+        }
+
+        worker.on('task-registred', (task) => {
+            assert.equal(worker.tasks().length, 1);
+            done();
+        });
+        worker.register(task);
+    });
+
+    it('tasks should be imutable', ()=>{
+        worker.tasks().push({});
+        assert.equal(worker.tasks().length, 0);
+    });
+});
