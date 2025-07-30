4bcfe934a4d1dc0f89992676005d630ab1f79bd8
Giovani Guizzo <giovaniguizzo@gmail.com>
2025-07-30 20:28:12 -0300

    refactor: update vitest configuration to include test setup and path resolution

--- packages/core/vitest.config.js
+++ packages/core/vitest.config.js
@@ -1,4 +1,5 @@
+import path from "path";
 import { defineConfig } from "vitest/config";
 import { createVitestConfig } from "../../vitest.base.config";
 
-export default defineConfig(createVitestConfig());
+export default defineConfig(createVitestConfig(["../../tests/vitest.setup.ts"], path.resolve("..", "..", "./tests")));
