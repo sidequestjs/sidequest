686afa5971b273b215bed553e6482c7249418977
Giovani Guizzo <giovani.guizzo@brickabode.com>
2025-08-11 09:30:56 -0300

    fix: set schema name for migrations based on searchPath in PostgresBackend (#67)
    * fix: set schema name for migrations based on searchPath in PostgresBackend
    
    * feat: add schema management to PostgresBackend with migration support
    
    * fix: simplify schema creation in migrate method by removing ensureSchemaExists

--- packages/backends/postgres/src/postgres-backend.ts
+++ packages/backends/postgres/src/postgres-backend.ts
@@ -39,14 +39,43 @@ const defaultKnexConfig = {
  * @param dbConfig - Database configuration - can be a connection string or limited Knex config
  */
 export default class PostgresBackend extends SQLBackend {
+  private schemaName?: string;
+
   constructor(dbConfig: string | SQLDriverConfig) {
     const knexConfig: Knex.Config = {
       ...defaultKnexConfig,
       ...(typeof dbConfig === "string" ? { connection: dbConfig } : dbConfig),
     };
 
+    let schemaName: string | undefined;
+    if (knexConfig.searchPath) {
+      schemaName = typeof knexConfig.searchPath === "string" ? knexConfig.searchPath : knexConfig.searchPath[0];
+      knexConfig.migrations!.schemaName = schemaName;
+    }
+
     const knex = createKnex(knexConfig);
     super(knex);
+
+    this.schemaName = schemaName;
+  }
+
+  /**
+   * Migrates the database by ensuring the required schema exists and then invoking the parent migration logic.
+   *
+   * @remarks
+   * If a schema name is specified, this method will attempt to create the schema if it does not already exist.
+   * After ensuring the schema, it delegates further migration steps to the parent class.
+   *
+   * @returns A promise that resolves when the migration process is complete.
+   */
+  async migrate(): Promise<void> {
+    // Create schema if it doesn't exist
+    if (this.schemaName) {
+      await this.knex.raw(`CREATE SCHEMA IF NOT EXISTS ??`, [this.schemaName]);
+    }
+
+    // Call parent migrate method
+    await super.migrate();
   }
 
   truncDate(date: string, unit: "m" | "h" | "d"): string {
