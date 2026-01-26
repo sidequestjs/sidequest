f8786184a808836b27ca70a2db85e6127a17662b
Giovani Guizzo <giovaniguizzo@gmail.com>
2026-01-26 20:21:40 -0300

    fix: enhance stale job handling with JobTransitioner and improve test coverage

--- tests/integration/sidequest.jobs.js
+++
@@ -1,3 +0,0 @@
-import { EnqueueFromWithinJob, FailingJob, RetryJob, SuccessJob, TimeoutJob } from "./jobs/test-jobs.js";
-
-export { EnqueueFromWithinJob, FailingJob, RetryJob, SuccessJob, TimeoutJob };
